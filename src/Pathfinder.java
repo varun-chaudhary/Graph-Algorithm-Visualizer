82a80e822578b4d65078838922912bd547cc9d53
Varun Chaudhary <varunc105@gmail.com>
2024-04-11 22:09:48 +0530

    bfs dfs added

--- src/Pathfinder.java
+++ src/Pathfinder.java
@@ -51,4 +51,73 @@ public class Pathfinder {
         return neighbors;
     }
 
+    public ArrayList<Node> bfs() {
+        this.path = new ArrayList<>();
+        Queue<Node> queue = new LinkedList<>();
+
+        queue.add(this.startNode);
+        this.grid[this.startNode.getY()][this.startNode.getX()].setVisited(true);
+        while (!queue.isEmpty()) {
+            if (this.grid[endNode.getY()][endNode.getX()].isVisited()) {
+                System.out.println("BFS: End found; breaking");
+                break;
+            }
+
+            this.currentNode = queue.poll();
+            for (Node neighbor : getNeighbors(this.currentNode)) {
+                if (neighbor != null && !neighbor.isVisited() && !neighbor.isBarrier()) {
+                    queue.add(neighbor);
+                    neighbor.setVisited(true);
+
+                    neighbor.setParent(this.currentNode);
+                    this.path.add(this.grid[neighbor.getY()][neighbor.getX()]);
+                }
+            }
+        }
+
+        return this.path;
+    }
+
+    public ArrayList<Node> dfs() {
+        this.path = new ArrayList();
+        dfsRecursive(this.startNode, null);
+        return this.path;
+    }
+
+    private void dfsRecursive(Node currentNode, Node parentNode) {
+        int currentX = currentNode.getX();
+        int currentY = currentNode.getY();
+        if (this.grid[endNode.getY()][endNode.getX()].isVisited()) {
+            System.out.println("DFS: End found; returning");
+            return;
+        } else if (currentY < 0 || currentY >= this.grid.length || currentX < 0
+                || currentX >= this.grid[currentY].length || this.grid[currentY][currentX].isBarrier()
+                || this.grid[currentY][currentX].isVisited()) { // Exit method if out of bounds or on closed node(?)
+            return;
+        }
+
+        this.grid[currentY][currentX].setVisited(true);
+        this.path.add(this.grid[currentY][currentX]);
+        if (parentNode != null) {
+            currentNode.setParent(parentNode);
+        }
+
+        try { // Up
+            dfsRecursive(this.grid[currentY - 1][currentX], currentNode);
+        } catch (ArrayIndexOutOfBoundsException e) {
+        }
+        try { // Right
+            dfsRecursive(this.grid[currentY][currentX + 1], currentNode);
+        } catch (ArrayIndexOutOfBoundsException e) {
+        }
+        try { // Down
+            dfsRecursive(this.grid[currentY + 1][currentX], currentNode);
+        } catch (ArrayIndexOutOfBoundsException e) {
+        }
+        try { // Left
+            dfsRecursive(this.grid[currentY][currentX - 1], currentNode);
+        } catch (ArrayIndexOutOfBoundsException e) {
+        }
+    }
+
 }
